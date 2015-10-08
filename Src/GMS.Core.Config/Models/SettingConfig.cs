4c6ed3e0f356ab1740951ea9bbc44e802d8aca96
王小龙 <1036199213@qq.com>
2015-10-08 20:25:13 +0800

    cms管理系统

---
+++ Src/GMS.Core.Config/Models/SettingConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GMS.Core.Config
+{
+    /// <summary>
+    /// 数据库配置
+    /// </summary>
+    [Serializable]
+    public class SettingConfig : ConfigFileBase
+    {
+        public SettingConfig()
+        {
+        }
+
+        #region 序列化属性
+        public String WebSiteTitle { get; set; }
+        public String WebSiteDescription { get; set; }
+        public String WebSiteKeywords { get; set; }
+
+        public List<Banner> Banners { get; set; }
+        #endregion
+    }
+
+    [Serializable]
+    public class Banner
+    {
+        public string BannerSrc { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ Src/GMS.Web.Admin/Areas/Config/Controllers/SiteSettingController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GMS.Web.Admin.Common;
+using GMS.Framework.Utility;
+using GMS.Framework.Web;
+using GMS.Account.Contract;
+using GMS.Framework.Contract;
+using GMS.Core.Config;
+
+namespace GMS.Web.Admin.Areas.Config.Controllers
+{
+    public class SiteSettingController : AdminControllerBase
+    {
+        //
+        // GET: /Config/SiteSetting/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            var adminMenuConfig = CachedConfigContext.Current.SettingConfig;
+            return View(adminMenuConfig);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(GMS.Core.Config.SettingConfig site)
+        {
+            try
+            {
+                var adminMenuConfig = CachedConfigContext.Current.SettingConfig;
+                adminMenuConfig.WebSiteDescription = site.WebSiteDescription ?? "";
+                adminMenuConfig.WebSiteKeywords = site.WebSiteKeywords ?? "";
+                adminMenuConfig.WebSiteTitle = site.WebSiteTitle ?? "";
+
+                CachedConfigContext.Current.Save(adminMenuConfig);
+
+                return RedirectToAction("index");
+            }
+            catch (Exception err)
+            {
+                return View("Error");
+            }
+            
+        }
+
+    }
+}
